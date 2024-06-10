bd640492785c564aeba5c51198eba2214ea3f530
Alexander Ilseman <al@ilseman.com>
2024-06-10 11:46:34 -0500

    Added get /

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('hello!')
+const express = require('express')
+import { Request, Response } from 'express'
+const app = express()
+const port = 3300
+
+
+app.get('/', (req: Request, res: Response) => { res.send('We did it again') }
+)
+
+app.listen(port, () => {
+    console.log(`ToDo API Running on port ${port}`)
+})
