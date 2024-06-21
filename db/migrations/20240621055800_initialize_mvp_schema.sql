97d07c1186dff564340ac7c0b2fe32e401fb1d73
Alexander Ilseman <al@ilseman.com>
2024-06-21 01:42:50 -0500

    refactored migration and wrote seed-data script

--- db/migrations/20240621055800_initialize_mvp_schema.sql
+++ db/migrations/20240621055800_initialize_mvp_schema.sql
@@ -3,10 +3,10 @@ CREATE TABLE account_profiles (
     profile_id SERIAL PRIMARY KEY,
     username VARCHAR(32),
     email VARCHAR(64) UNIQUE NOT NULL,
-    "password" TEXT NOT NULL
+    pwd TEXT NOT NULL
 );
 
-CREATE TABLE "app_users" (
+CREATE TABLE app_users (
     user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(64) NOT NULL,
     last_name VARCHAR(64) NOT NULL,
@@ -17,15 +17,15 @@ CREATE TABLE "app_users" (
 CREATE TABLE todos (
     todo_id SERIAL PRIMARY KEY,
     title VARCHAR(128),
-    "body" TEXT,
+    body TEXT,
     user_id INTEGER REFERENCES app_users
 );
 
 CREATE TABLE tags (
     tag_id SERIAL PRIMARY KEY,
-    "label" VARCHAR(32) NOT NULL,
+    label VARCHAR(32) NOT NULL,
     user_id INTEGER REFERENCES app_users NOT NULL,
-    UNIQUE (user_id, "label")
+    UNIQUE (user_id, label)
 );
 
 CREATE TABLE todo_tags (
@@ -38,5 +38,5 @@ CREATE TABLE todo_tags (
 DROP TABLE IF EXISTS todo_tags;
 DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS todos;
-DROP TABLE IF EXISTS "app_users";
+DROP TABLE IF EXISTS app_users;
 DROP TABLE IF EXISTS account_profiles;
---
+++ db/seeds/dev.data.sql
@@ -0,0 +1,36 @@
+-- This makes a good case for using variables in the SQL
+INSERT INTO account_profiles (username, email, pwd)
+VALUES ('johndoe', 'actualize-grad-colab-testuser@gmail.com', 'password123');
+
+INSERT INTO app_users (first_name, last_name, display_name, profile_id)
+SELECT
+    'John' AS first_name,
+    'Doe' AS last_name,
+    'JDoe' AS display_name,
+    profile_id
+FROM account_profiles
+WHERE username = 'jondoe';
+
+INSERT INTO todos (title, body, user_id)
+SELECT
+    'Buy milk' AS title,
+    'Need to buy milk from the store' AS body,
+    user_id
+FROM app_users
+WHERE display_name = 'JDoe';
+
+INSERT INTO tags (label, user_id)
+SELECT
+    'Groceries' AS label,
+    user_id
+FROM app_users WHERE display_name = 'JDoe';
+
+INSERT INTO todo_tags (todo_id, tag_id)
+SELECT
+    todos.todo_id,
+    tags.tag_id
+FROM todos
+CROSS JOIN tags
+WHERE
+    todos.title = 'Buy milk'
+    AND tags.label = 'Groceries';
